099f89a2e38acabf17aaff55f888624d13d129c5
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2023-08-15 11:59:35 -0500

    fixed some codes

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-
-
 const app = express();
+//only this "views" can be named however.
+app.set('views', path.join(__dirname, 'views')); //the first "views" is an ejs word, you cant just name it.
+app.set('view engine', 'ejs'); //"view engine" is an ejs word, you can't just name it.
 app.use(express.static('public'));//allowing ExpressJS to access and display CSS/JS files
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 //main page
-app.get('/', function(req, res){
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    res.sendFile(htmlFilePath);
+app.get('/', function (req, res) {
+    res.render('index'); // no need to add the .ejs, since it will auto look for it. Check line 7.
 });
 //restaurants page
-app.get('/restaurants', function(req, res){
-    const htmlFilePath = path.join(__dirname, 'views','restaurants.html');
-    res.sendFile(htmlFilePath);
+app.get('/restaurants', function (req, res) {
+
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, 
+        restaurants: storedRestaurants });
+    //old expressjs code, keep it for reference in the future..
+    //const htmlFilePath = path.join(__dirname, 'views','restaurants.html');
+    //res.sendFile(htmlFilePath);
 });
 //recommend page reading
-app.get('/recommend', function(req,res){
-    const htmlFilePath = path.join(__dirname, 'views','recommend.html');
-    res.sendFile(htmlFilePath);
+app.get('/recommend', function (req, res) {
+    res.render('recommend');
 });
 //recommend page writing
-app.post('/recommend',function(req,res){
+app.post('/recommend', function (req, res) {
     const restaurant = req.body;
-    const filePath = path.join(__dirname,'data','restaurants.json');
-    
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
     const fileData = fs.readFileSync(filePath);
     const storedRestaurants = JSON.parse(fileData);
     storedRestaurants.push(restaurant);
@@ -33,14 +41,12 @@ app.post('/recommend',function(req,res){
     res.redirect('/confirm');
 });
 //confirm page
-app.get('/confirm', function(req,res){
-    const htmlFilePath = path.join(__dirname, 'views','confirm.html');
-    res.sendFile(htmlFilePath);
+app.get('/confirm', function (req, res) {
+    res.render('confirm');
 });
 //about page
-app.get('/about', function(req,res){
-    const htmlFilePath = path.join(__dirname, 'views','about.html');
-    res.sendFile(htmlFilePath);
+app.get('/about', function (req, res) {
+    res.render('about');
 });
 
 app.listen(3000);
--- readme.md
+++ readme.md
@@ -4,3 +4,7 @@ ASSUMING you already installed nodejs on your local machine, <br>
 you can just download everything within this project, EXCEPT node_modules folder.
 <br>
 Then once you have everything, open console and use npm install to download all packages/dependencies
+<br>
+CURRENT PACKAGES: express, nodemon, ejs.
+<br>
+Install extensions in VS-Code: express, ejs, nodejs.
